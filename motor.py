85259d8fe733d6f5488e621d4f68827cae5594a8
Laeti <laetitia.cabrol@etu.u-bordeaux.fr>
2022-10-05 17:11:56 +0200

    test

--- motor.py
+++ motor.py
@@ -7,10 +7,10 @@ ports = dm.get_available_ports()
 if not ports:
     exit('No port')
 dxl_io = dm.DxlIO(ports[0])
-motor_ids=dxl_io(ports[0]).scan()
+#motor_ids=dxl_io(ports[0]).scan()
 dxl_io.set_wheel_mode([5])
-dxl_io.set_moving_speed({5: 360}) # Degrees / s'''
-dxl_io.set_moving_speed({2: -360})
+dxl_io.set_moving_speed({5: -360}) # Degrees / s'''
+dxl_io.set_moving_speed({2: 360})
 time.sleep(2)
 
 dxl_io.set_moving_speed({2: 0}) 
