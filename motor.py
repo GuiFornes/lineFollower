cc7b126aa0459c2f056007e72aaa48c1eed443d1
Laeti <laetitia.cabrol@etu.u-bordeaux.fr>
2022-10-05 17:51:02 +0200

    test

--- motor.py
+++ motor.py
@@ -10,8 +10,8 @@ dxl_io = dm.DxlIO(ports[0])
 #motor_ids=dxl_io(ports[0]).scan()
 dxl_io.set_wheel_mode([5])
 dxl_io.set_wheel_mode([2]) #gauche
-dxl_io.set_moving_speed({5: -360}) # Degrees / s'''
-dxl_io.set_moving_speed({2: 360})
+dxl_io.set_moving_speed({5: 1000}) # Degrees / s'''
+dxl_io.set_moving_speed({2: 1000})
 time.sleep(2)
 
 dxl_io.set_moving_speed({2: 0}) 
