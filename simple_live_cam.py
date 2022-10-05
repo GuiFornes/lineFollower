33104eec1dee5fc8ade80d6ef487c2d698f3e93d
GuiFornes <fornesguillaume@gmail.com>
2022-10-05 10:15:02 +0200

    simple cam viewer, can save picture

---
+++ simple_live_cam.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+if __name__ == "__main__":
+    cam = cv2.VideoCapture(2)
+
+    while True:
+        ret, frame = cam.read()
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.imwrite("frame.png", frame)
+            break
