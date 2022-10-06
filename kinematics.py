9fa16631c2b06f8d3aa5b712425b533c3378082d
GuiFornes <fornesguillaume@gmail.com>
2022-10-06 16:13:51 +0200

    follow line

--- kinematics.py
+++ kinematics.py
@@ -25,14 +25,14 @@ def odom(linear, angular, t):
     :param t: time (s)
     :return: dx, dy, dtheta (m, m, rad)
     """
-    print(f"lin, ang : {linear:.5f}, {angular:.5f}")
+    # print(f"lin, ang : {linear:.5f}, {angular:.5f}")
     if angular == 0:
         dx, dy, dtheta = 0, linear * t, 0
     else:
         dx = (linear / angular) * (1 - np.cos(angular * t))
         dy = (linear / angular) * np.sin(angular * t)
         dtheta = angular * t
-    print(f"vitesse : {(dy/t)*100:.1f}")
+    # print(f"vitesse : {(dy/t)*100:.1f}")
     return dx*20, dy*20, dtheta
 
 
