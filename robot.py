8bf0c04e80da0fbf790491a8f572e9289955ecac
GuiFornes <fornesguillaume@gmail.com>
2022-10-06 11:41:12 +0200

    getpresent vs getmoving

--- robot.py
+++ robot.py
@@ -118,7 +118,7 @@ class Robot:
             # Update robot information
             # print("[INFO] Updating robot information")
             # print("[INFO] Reading encoders : ", self.dxl_io.get_present_speed((2, 5)))
-            speedL, speedR = self.dxl_io.get_present_speed([2, 5])
+            speedL, speedR = self.dxl_io.get_moving_speed([2, 5])
 
             self.odom.rot_speedL = utils.deg_to_rad_second(speedL)
             self.odom.rot_speedR = utils.deg_to_rad_second(-speedR)
