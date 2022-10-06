d5cf2d34d2e3de2e63c8c3bbc63516f9c2123979
GuiFornes <fornesguillaume@gmail.com>
2022-10-06 09:33:51 +0200

    colooooor

--- constants.py
+++ constants.py
@@ -7,11 +7,11 @@ RADIUS = 0.025  # radius of the wheels (m)
 L = 0.12  # Distance between the wheels (m)
 
 ORIGIN = 320, 480  # Origin of the robot (pixels)
-SCALE = 0.042/640, 0.039/480  # Scale of the camera (m/pixel) 0.042/640
+SCALE = 0.047/640, 0.036/480  # Scale of the camera (m/pixel) 0.042/640
 
 BLUE, RED, GREEN = 0, 1, 2  # Green not programmed yet in vision part
 COLOR_BOUND = [  # HSV
-    (np.array([0, 40, 85]), np.array([80, 255, 255])),  # BLUE
-    (np.array([45, 40, 0]), np.array([100, 255, 255])),  # RED
-    (np.array([100, 75, 65]), np.array([120, 185, 255])),  # GREEN
+    (np.array([89, 122, 83]), np.array([142, 255, 241])),  # BLUE
+    (np.array([0, 72, 0]), np.array([15, 255, 255])),  # RED
+    (np.array([27, 36, 80]), np.array([86, 255, 238])),  # GREEN
 ]
