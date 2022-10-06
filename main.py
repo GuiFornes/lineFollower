44bbe97e9bc5c800371b5f18ac5c9e4d1089710c
GuiFornes <fornesguillaume@gmail.com>
2022-10-06 14:21:36 +0200

    main print better

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ print("[INFO] FirstBot is ready to go")
 follow = False
 
 if follow:
-    t_robot = time.time()
     try:
         firstBot.follow_line(color=GREEN)
     except KeyboardInterrupt:
         print("[INFO] Stopping robot, bye bye")
-        print("time robot = ", time.time()-t_robot)
-        print("time_thread = ", firstBot.time_thread)
     finally:
-        print("time robot = ", time.time()-t_robot)
-        print("time_thread = ", firstBot.time_thread)
         firstBot.set_speed(0, 0)
         firstBot.compliant()
 
 else:
     firstBot.compliant()
-    while True:
-        print("[INFO] motor speed: ", firstBot.get_real_speed())
-        print("[INFO] position: ", firstBot.get_location())
-        time.sleep(0.5)
+    t_robot = time.time()
+    try:
+        while True:
+            print("[INFO] motor speed: ", firstBot.get_real_speed())
+            print("[INFO] position: ", firstBot.get_location())
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("time robot = ", time.time() - t_robot)
+        print("time_thread = ", firstBot.time_thread)
