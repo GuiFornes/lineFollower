d1bacb0caef5d448e141c59c9fe09abd249c0d43
gperron0 <gael.perron@hotmail.fr>
2022-10-06 17:14:19 +0200

    add test for goto

--- test_goto.py
+++ test_goto.py
@@ -10,11 +10,11 @@ firstBot = Robot()
 print("[INFO] FirstBot is ready to go")
 
 try:
-        firstBot.kin.go_to_xya(*firstBot.odom.position, firstBot.odom.orientation, 0.1, 0 , 0)
-    except KeyboardInterrupt:
-        print("[INFO] Stopping robot, bye bye")
-    finally:
-        firstBot.set_speed(0, 0)
-        firstBot.compliant()
+    firstBot.kin.go_to_xya(*firstBot.odom.position, firstBot.odom.orientation, 0.1, 0 , 0)
+except KeyboardInterrupt:
+    print("[INFO] Stopping robot, bye bye")
+finally:
+    firstBot.set_speed(0, 0)
+    firstBot.compliant()
 
 
